9e00a781eab72515bbc0974a2ba7c5590af956f9
arsen <arsen@LAPTOP-K7E9IAPG>
2020-05-24 11:35:29 +0900

    >>20.05.24 커밋 & 푸시

---
+++ src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -0,0 +1,158 @@
+package com.springbook.biz.board.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.common.JDBCUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+@Repository("boardDAO")
+//DAO(DATA ACCESS OBJECT)
+public class BoardDAO {
+	// JDBC 관련 변수
+	private Connection conn = null;
+	private PreparedStatement stmt = null;
+	private ResultSet rs = null;
+
+	// SQL 명령어들
+
+	private final String BOARD_INSERT = "insert into board(seq, title, writer, content) values((select "
+			+ "nvl(max(seq),0)+1 from board),?,?,?)";
+	private final String BOARD_UPDATE = "update board set title=?, content=? where seq=?";
+	private final String BOARD_DELETE = "delete board where seq=?";
+	private final String BOARD_GET = "select * from board where seq=?";
+	private final String BOARD_LIST = "select * from board order by seq desc";
+
+	// CRUD 기능의 메소드 구현
+	// 글 등록
+	/* (non-Javadoc)
+	 * @see com.springbook.biz.board.impl.BoardService#insertBoard(com.springbook.biz.board.BoardVO)
+	 */
+	public BoardDAO(){
+		System.out.println("BoardDAO객체실행");
+	}
+	public void insertBoard(BoardVO vo) {
+		System.out.println("===> JDBC로 insertBoard() 기능 처리");
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_INSERT);
+			stmt.setString(1, vo.getTitle());
+			stmt.setString(2, vo.getWriter());
+			stmt.setString(3, vo.getContent());
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(stmt, conn);
+		}
+	}
+
+	// 글 수정
+	/* (non-Javadoc)
+	 * @see com.springbook.biz.board.impl.BoardService#updateBoard(com.springbook.biz.board.BoardVO)
+	 */
+	
+	public void updateBoard(BoardVO vo) {
+		System.out.println("===> JDBC로 updateBoard() 기능 처리");
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_UPDATE);
+			stmt.setString(1, vo.getTitle());
+			stmt.setString(2, vo.getContent());
+			stmt.setInt(3, vo.getSeq());
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(stmt, conn);
+		}
+
+	}
+
+	// 글 삭제
+	/* (non-Javadoc)
+	 * @see com.springbook.biz.board.impl.BoardService#deleteBoard(com.springbook.biz.board.BoardVO)
+	 */
+	
+	public void deleteBoard(BoardVO vo) {
+		System.out.println("===>JDBC로 deleteBoard() 기능 처리");
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_DELETE);
+			stmt.setInt(1, vo.getSeq());
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(stmt, conn);
+		}
+
+	}
+
+	// 글 상세 조회
+	/* (non-Javadoc)
+	 * @see com.springbook.biz.board.impl.BoardService#getBoard(com.springbook.biz.board.BoardVO)
+	 */
+	
+	public BoardVO getBoard(BoardVO vo) {
+		System.out.println("===> JDBC로 getBoard() 기능 처리");
+		BoardVO board = null;
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_GET);
+			stmt.setInt(1, vo.getSeq());
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				board = new BoardVO();
+				board.setSeq(rs.getInt("SEQ"));
+				board.setTitle(rs.getString("TITLE"));
+				board.setWriter(rs.getString("WRITER"));
+				board.setContent(rs.getString("CONTENT"));
+				board.setRegDate(rs.getDate("REGDATE"));
+				board.setCnt(rs.getInt("CNT"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		return board;
+	}
+
+	// 글 목록 조회
+	/* (non-Javadoc)
+	 * @see com.springbook.biz.board.impl.BoardService#getBoardList(com.springbook.biz.board.BoardVO)
+	 */
+
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		System.out.println("===> JDBC로 getBoardList() 기능 처리");
+		List<BoardVO> boardList = new ArrayList<BoardVO>();
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_LIST);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				BoardVO board = new BoardVO();
+				board.setSeq(rs.getInt("SEQ"));
+				board.setTitle(rs.getString("TITLE"));
+				board.setWriter(rs.getString("WRITER"));
+				board.setContent(rs.getString("CONTENT"));
+				board.setRegDate(rs.getDate("REGDATE"));
+				board.setCnt(rs.getInt("CNT"));
+				boardList.add(board);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		return boardList;
+
+	}
+}
---
+++ src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.springbook.biz.user.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.springbook.biz.user.UserVO;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+	/*@Autowired
+	@Qualifier("userDAO")*/
+	@Resource(name="userDAO")
+	private UserDAO userDAO;
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	@Override
+	public UserVO getUser(UserVO vo) {
+		// TODO Auto-generated method stub
+		return userDAO.getUser(vo);
+	}
+	
+}
